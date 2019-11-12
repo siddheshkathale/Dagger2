73a1c954eb47753ee488a60cf11d6b6a9d2b94ae
Siddhesh <siddheshkathale@gmail.com>
2019-11-12 15:09:06 +0530

    modofied MainActivity with json object

--- app/src/main/java/com/demo/dagger2demo/MainActivity.kt
+++ app/src/main/java/com/demo/dagger2demo/MainActivity.kt
@@ -2,12 +2,33 @@ package com.demo.dagger2demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.FileWriter
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val obj = JSONObject()
+        obj.put("Name", "crunchify.com")
+        obj.put("Author", "App Shah")
+
+        val company = JSONArray()
+        company.put("Compnay: eBay")
+        company.put("Compnay: Paypal")
+        company.put("Compnay: Google")
+        obj.put("Company List", company)
+
+        // try-with-resources statement based on post comment below :)
+        FileWriter("/Users/<username>/Documents/file1.txt").use({ file ->
+            file.write(obj.toString())
+            println("Successfully Copied JSON Object to File...")
+            println("\nJSON Object: $obj")
+        })
     }
 
     fun testUpload(){
@@ -15,4 +36,6 @@ class MainActivity : AppCompatActivity() {
         var i = 1 + 1
 
     }
+
+
 }
